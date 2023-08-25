11faef5de16846c766b9dd1b818a50e14bd4d713
kagithapk <kagithapk@gmail.com>
2023-08-26 03:22:08 +0530

    fix: module path

--- src/components/Button/index.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import './style.css';
-export const Button = ({ onClick }) => {
-    return (_jsx("button", { onClick: onClick, className: "button", children: "Hello" }));
-};
